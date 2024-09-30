ef1a1ddfc741d4df74da992c5f836a85e85e6618
Ben <ambenard2002@gmail.com>
2024-09-30 12:52:41 +0000

    Update Date handling

--- trial.js
+++ trial.js
@@ -1,7 +1,10 @@
-const a = [1,2,3,4]
+//test code
+// const a = [1,2,3,4]
 
-a.splice(1,1)
+// a.splice(1,1)
 
-const date = new Date()
-const month = date.toLocaleString('default', { month: 'long' });
-console.log(month);
+// const date = new Date()
+// const month = date.toLocaleString('default', { month: 'long' });
+// // console.log(month, date.getDate(), date.getFullYear());
+
+// console.log(new Date().toLocaleString('default', { month: 'long' }), new Date().getDate(), new Date().getFullYear());
